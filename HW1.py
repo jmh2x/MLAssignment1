b053019237222162e2171badbbd80b700fd8b3d9
jmh2x <142468976+jmh2x@users.noreply.github.com>
2026-02-02 14:54:28 -0500

    Refactor comments and improve code clarity

--- HW1.py
+++ HW1.py
@@ -1,60 +1,71 @@
-#Govan Henry HW1 CMSC438  1/30/26
+#Govan Henry    H01    CMSC438    1/30/26
 import numpy as np
 
 def dotting(f):
-    # Initialize variables and dotted variables
     var = [] #store variables
     dotted = [] #store dotted
 
     for instruction in f: #iterate through each instruction
-        op = instruction[0] #get operation type
+        op = instruction[0] #operation type
 
-        if op == 'X':  # Input x
+        if op == 'X': #x
             value = instruction[1]
             var.append(value)
             dotted.append(1.0)
 
-        elif op == 'C':  # Constant
+        
+        elif op == 'C':#constant
             value = instruction[1]
             var.append(value)
             dotted.append(0.0)
 
-        elif op == '+':  # Addition
+        
+        elif op == '+':#Add
             u, v = instruction[1], instruction[2]
             var.append(var[u] + var[v])
             dotted.append(dotted[u] + dotted[v])
 
-        elif op == '-':  # Subtraction
+        
+        elif op == '-':#subtract
             u, v = instruction[1], instruction[2]
             var.append(var[u] - var[v])
             dotted.append(dotted[u] - dotted[v])
 
-        elif op == '*':  # Multiplication
+        
+        elif op == '*':#multiply
             u, v = instruction[1], instruction[2]
             var.append(var[u] * var[v])
             dotted.append(dotted[u] * var[v] + var[u] * dotted[v])
 
-        elif op == '/':  # Division
+
+        
+        elif op == '/':#divide
             u, v = instruction[1], instruction[2]
             var.append(var[u] / var[v])
             dotted.append((dotted[u] * var[v] - var[u] * dotted[v]) / (var[v] ** 2))
 
-        elif op == 'S':  # Square
+        
+        elif op == 'S':#^2
             u = instruction[1]
             var.append(var[u] ** 2)
             dotted.append(2.0 * var[u] * dotted[u])
 
-        elif op == 'E':  # Exponential
+        
+        elif op == 'E':#^x
             u = instruction[1]
             var.append(np.exp(var[u]))
             dotted.append(np.exp(var[u]) * dotted[u])
 
-        elif op == 'L':  # Logarithm
+
+        
+        elif op == 'L':#log
             u = instruction[1]
             var.append(np.log(var[u]))
             dotted.append(dotted[u] / var[u])
 
-    # Return x, y, and dotted_y
+
+    
+    #eeturn x, y, and dotted y
     x = var[0]
     y = var[-1]
     dotted_y = dotted[-1]
